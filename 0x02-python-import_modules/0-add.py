4d15e769fb5a0ce993595f355990a91d5875e791
kafilat1210 <cafylhearty@gmail.com>
2024-01-05 03:54:14 -0800

    Function import

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    """Print the sum of 1 and 2."""
-    from add_0 import add
-
+    import add_0 as mod1
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
-
+    print("{} + {} = {}".format(a, b, mod1.add(a, b)))
