0414d3635fe556d700bacb04534b40d6ce8eea60
kafilat1210 <122450001+kafilat1210@users.noreply.github.com>
2024-01-06 18:37:20 +0100

    Delete 0x01-python-if_else_loops_functio directory

--- 0x01-python-if_else_loops_functio/3-print_alphabt.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-for i in "abcdefghijklmnopqrstuvwxyz":
-    if i in "qe":
-        continue
-    print("{0}".format(i), end="")
--- 0x01-python-if_else_loops_functio/8-uppercase.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def uppercase(str):
-    result = ""
-    for char in str:
-        if 97 <= ord(char) <= 122:
-            result += chr(ord(char) - 32)
-        else:
-            result += char
-    print("{}".format(result))
